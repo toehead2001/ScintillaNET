4628841b8cf9a2a7f4cd4b0816ed8a112f07c0a6
toehead2001 <toehead2001@users.noreply.github.com>
2019-04-15 13:28:09 -0600

    Update types in the SCNotification struct

--- src/ScintillaNET/Scintilla.cs
+++ src/ScintillaNET/Scintilla.cs
@@ -2095,14 +2095,14 @@ namespace ScintillaNET
         private void ScnDoubleClick(ref NativeMethods.SCNotification scn)
         {
             var keys = Keys.Modifiers & (Keys)(scn.modifiers << 16);
-            var eventArgs = new DoubleClickEventArgs(this, keys, scn.position, scn.line);
+            var eventArgs = new DoubleClickEventArgs(this, keys, scn.position.ToInt32(), scn.line.ToInt32());
             OnDoubleClick(eventArgs);
         }
 
         private void ScnHotspotClick(ref NativeMethods.SCNotification scn)
         {
             var keys = Keys.Modifiers & (Keys)(scn.modifiers << 16);
-            var eventArgs = new HotspotClickEventArgs(this, keys, scn.position);
+            var eventArgs = new HotspotClickEventArgs(this, keys, scn.position.ToInt32());
             switch (scn.nmhdr.code)
             {
                 case NativeMethods.SCN_HOTSPOTCLICK:
@@ -2125,11 +2125,11 @@ namespace ScintillaNET
             {
                 case NativeMethods.SCN_INDICATORCLICK:
                     var keys = Keys.Modifiers & (Keys)(scn.modifiers << 16);
-                    OnIndicatorClick(new IndicatorClickEventArgs(this, keys, scn.position));
+                    OnIndicatorClick(new IndicatorClickEventArgs(this, keys, scn.position.ToInt32()));
                     break;
 
                 case NativeMethods.SCN_INDICATORRELEASE:
-                    OnIndicatorRelease(new IndicatorReleaseEventArgs(this, scn.position));
+                    OnIndicatorRelease(new IndicatorReleaseEventArgs(this, scn.position.ToInt32()));
                     break;
             }
         }
@@ -2137,7 +2137,7 @@ namespace ScintillaNET
         private void ScnMarginClick(ref NativeMethods.SCNotification scn)
         {
             var keys = Keys.Modifiers & (Keys)(scn.modifiers << 16);
-            var eventArgs = new MarginClickEventArgs(this, keys, scn.position, scn.margin);
+            var eventArgs = new MarginClickEventArgs(this, keys, scn.position.ToInt32(), scn.margin);
 
             if (scn.nmhdr.code == NativeMethods.SCN_MARGINCLICK)
                 OnMarginClick(eventArgs);
@@ -2153,7 +2153,7 @@ namespace ScintillaNET
 
             if ((scn.modificationType & NativeMethods.SC_MOD_INSERTCHECK) > 0)
             {
-                var eventArgs = new InsertCheckEventArgs(this, scn.position, scn.length, scn.text);
+                var eventArgs = new InsertCheckEventArgs(this, scn.position.ToInt32(), scn.length.ToInt32(), scn.text);
                 OnInsertCheck(eventArgs);
 
                 cachedPosition = eventArgs.CachedPosition;
@@ -2165,7 +2165,7 @@ namespace ScintillaNET
             if ((scn.modificationType & (NativeMethods.SC_MOD_BEFOREDELETE | NativeMethods.SC_MOD_BEFOREINSERT)) > 0)
             {
                 var source = (ModificationSource)(scn.modificationType & sourceMask);
-                var eventArgs = new BeforeModificationEventArgs(this, source, scn.position, scn.length, scn.text);
+                var eventArgs = new BeforeModificationEventArgs(this, source, scn.position.ToInt32(), scn.length.ToInt32(), scn.text);
 
                 eventArgs.CachedPosition = cachedPosition;
                 eventArgs.CachedText = cachedText;
@@ -2186,7 +2186,7 @@ namespace ScintillaNET
             if ((scn.modificationType & (NativeMethods.SC_MOD_DELETETEXT | NativeMethods.SC_MOD_INSERTTEXT)) > 0)
             {
                 var source = (ModificationSource)(scn.modificationType & sourceMask);
-                var eventArgs = new ModificationEventArgs(this, source, scn.position, scn.length, scn.text, scn.linesAdded);
+                var eventArgs = new ModificationEventArgs(this, source, scn.position.ToInt32(), scn.length.ToInt32(), scn.text, scn.linesAdded.ToInt32());
 
                 eventArgs.CachedPosition = cachedPosition;
                 eventArgs.CachedText = cachedText;
@@ -2211,7 +2211,7 @@ namespace ScintillaNET
 
             if ((scn.modificationType & NativeMethods.SC_MOD_CHANGEANNOTATION) > 0)
             {
-                var eventArgs = new ChangeAnnotationEventArgs(scn.line);
+                var eventArgs = new ChangeAnnotationEventArgs(scn.line.ToInt32());
                 OnChangeAnnotation(eventArgs);
             }
         }
@@ -2802,7 +2802,7 @@ namespace ScintillaNET
                         break;
 
                     case NativeMethods.SCN_STYLENEEDED:
-                        OnStyleNeeded(new StyleNeededEventArgs(this, scn.position));
+                        OnStyleNeeded(new StyleNeededEventArgs(this, scn.position.ToInt32()));
                         break;
 
                     case NativeMethods.SCN_SAVEPOINTLEFT:
@@ -2827,11 +2827,11 @@ namespace ScintillaNET
                         break;
 
                     case NativeMethods.SCN_AUTOCSELECTION:
-                        OnAutoCSelection(new AutoCSelectionEventArgs(this, scn.position, scn.text, scn.ch, (ListCompletionMethod)scn.listCompletionMethod));
+                        OnAutoCSelection(new AutoCSelectionEventArgs(this, scn.position.ToInt32(), scn.text, scn.ch, (ListCompletionMethod)scn.listCompletionMethod));
                         break;
 
                     case NativeMethods.SCN_AUTOCCOMPLETED:
-                        OnAutoCCompleted(new AutoCSelectionEventArgs(this, scn.position, scn.text, scn.ch, (ListCompletionMethod)scn.listCompletionMethod));
+                        OnAutoCCompleted(new AutoCSelectionEventArgs(this, scn.position.ToInt32(), scn.text, scn.ch, (ListCompletionMethod)scn.listCompletionMethod));
                         break;
 
                     case NativeMethods.SCN_AUTOCCANCELLED:
@@ -2843,11 +2843,11 @@ namespace ScintillaNET
                         break;
 
                     case NativeMethods.SCN_DWELLSTART:
-                        OnDwellStart(new DwellEventArgs(this, scn.position, scn.x, scn.y));
+                        OnDwellStart(new DwellEventArgs(this, scn.position.ToInt32(), scn.x, scn.y));
                         break;
 
                     case NativeMethods.SCN_DWELLEND:
-                        OnDwellEnd(new DwellEventArgs(this, scn.position, scn.x, scn.y));
+                        OnDwellEnd(new DwellEventArgs(this, scn.position.ToInt32(), scn.x, scn.y));
                         break;
 
                     case NativeMethods.SCN_DOUBLECLICK:
@@ -2855,7 +2855,7 @@ namespace ScintillaNET
                         break;
 
                     case NativeMethods.SCN_NEEDSHOWN:
-                        OnNeedShown(new NeedShownEventArgs(this, scn.position, scn.length));
+                        OnNeedShown(new NeedShownEventArgs(this, scn.position.ToInt32(), scn.length.ToInt32()));
                         break;
 
                     case NativeMethods.SCN_HOTSPOTCLICK:
